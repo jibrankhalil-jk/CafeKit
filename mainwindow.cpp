0404d9a0eb209ee411941cb627df72256e954843
jibrankhalil-jk <dev.jibran.khalil@gmail.com>
2024-03-14 21:56:58 +0500

    ui bugs updated

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "home.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,9 +16,21 @@ MainWindow::~MainWindow()
 }
 void MainWindow::on_pushButton_clicked()
 {
+    // QString id = "admin";
+    // QString password = "admin";
+
+    // if(ui->id->text() == id && ui->password->text() == password){
+    //     MainWindow::close();
+    //     Home* home = new Home();
+    //     home->show();
+    // }else{
+    //     QMessageBox::critical(this, "Error", "Wrong Password or id try again.");
+    // }
+
 
     MainWindow::close();
     Home* home = new Home();
-    // home->setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
     home->show();
+
+
 }
